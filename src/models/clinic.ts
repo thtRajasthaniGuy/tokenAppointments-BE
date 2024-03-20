6c2427eee625d061c4b160fcc0c26c015e586fd4
govindITT <Govind.bagla@idla.k12.id.us>
2024-03-20 11:55:20 +0530

    remove commented code

--- src/models/clinic.ts
+++ src/models/clinic.ts
@@ -44,29 +44,6 @@ const Clinic = new Schema(
   { collection: "Clinic" }
 );
 
-// Clinic.pre("save", async function (next) {
-//   if (!this.id) {
-//     try {
-//       // Generate the custom ID
-//       const id = await generateCustomId("Clinic1234567890", 10);
-//       this.id = id;
-//     } catch (error) {
-//       console.error("Error generating ID:", error);
-//       next(error); // Pass error to the next middleware
-//     }
-//   }
-//   if (this.isModified("password")) {
-//     return next();
-//   }
-//   bcrypt.hash(this.password, saltRounds, function (err, hash) {
-//     if (err) {
-//       return next(err);
-//     }
-//     this.password = hash;
-//     next();
-//   });
-// });
-
 Clinic.pre("save", async function (next) {
   const clinic = this; // Store a reference to 'this' context
 
