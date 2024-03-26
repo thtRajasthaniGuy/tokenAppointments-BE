6d2ed98c5a3a7d1a9a1349942f03017eaea916b8
sagarbagra <sagar.bagra199729@gmail.com>
2024-03-26 13:49:11 +0530

    driver login post

--- src/routes/doctorToken.ts
+++ src/routes/doctorToken.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import { isUserLogin } from "../utils/jwt";
-import { NextUserToken } from "../controllers/doctorToken";
+import { NextUserToken,GetDoctorTokens } from "../controllers/doctorToken";
 const router = express.Router();
 router.route("/next-user").post(isUserLogin, NextUserToken);
+router.route("/get-doctor-token/:clinicId").get(isUserLogin,GetDoctorTokens );
 
 export { router };
